1c1f71e6fa290e96be79ec0c0761607629b878e2
Hassan.Ragab <Hassan.Ragab@thermofisher.com>
2022-01-31 13:05:50 +0100

    add J,L,O, and S blocks

---
+++ Tetris/Blocks/OBlock.cs
@@ -0,0 +1,16 @@
+﻿namespace Tetris.Blocks
+{
+    public class OBlock : Block
+    {
+        public override int Id => 4;
+        protected override Position[][] Tiles => m_tiles;
+        protected override Position StartOffset => new Position(0, 4);
+
+        #region private fields and constants
+        private readonly Position[][] m_tiles = new Position[][]
+        {
+            new Position[] { new(0, 0), new(0, 1), new(1, 0), new(1, 1) },
+        };
+        #endregion
+    }
+}
